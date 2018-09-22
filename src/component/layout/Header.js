e8f9a250cff1e87a3ca34d984249472d3352c1ae
Vukasin Stojanovic <vultashin76@gmail.com>
2018-09-22 18:41:27 +0200

    chagned padding

--- src/component/layout/Header.js
+++ src/component/layout/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 class Header extends React.Component {
     render(){
         return(
-        <div className="text-center p-5 pb-0">
+        <div className="text-center pt-5 pb-0">
             <h1 style={{fontWeight: '100', color: '#233443'}}>Your<span style={{fontWeight: '500'}}>Story</span></h1>
             <p className="text-secondary">A place to share your story's and confessions</p>
         </div>
